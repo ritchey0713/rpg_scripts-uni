6bc50fb8bf0ee9c412eebd5e67593fabb4173c63
maveryk <ritchey0713@gmail.com>
2021-02-07 14:17:44 -0500

    add char attrs

--- CharStats.cs
+++ CharStats.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class CharStats : MonoBehaviour {
 	// Start is called before the first frame update
+
+	public string charName;
+	public int playerLevel = 1;
+	public int currentExp;
+
+
+
+	public int currentHP;
+	public int maxHP = 100;
+	public int currentMP;
+	public int maxMP = 30;
+
+	public int strength;
+	public int defense;
+	public int wpnPwr;
+	public int armrPwr; 
+
+	public string equippedWeapon;
+	public string equippedArmor;
+	public Sprite charImage;
 	void Start() {
 			
 	}
