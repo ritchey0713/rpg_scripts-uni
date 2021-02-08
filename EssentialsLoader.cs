73285ec2c65a54fd3ea1517938571fd59774c517
maveryk <ritchey0713@gmail.com>
2021-02-08 16:16:50 -0500

    create game menu

--- EssentialsLoader.cs
+++ EssentialsLoader.cs
@@ -11,13 +11,13 @@ public class EssentialsLoader : MonoBehaviour {
 	// Start is called before the first frame update
 	void Start() {
 		if(UIFade.instance == null) {
+			// called this way as we need to instantiate the fade since the area entrance calls it
 			UIFade.instance = Instantiate(UIScreen).GetComponent<UIFade>();
-			//Instantiate(UIScreen);
 		}
 
 		if(PlayerController.instance == null) {
 			// instantiate a player, and assign a new playercontroller to the variable clone
-			PlayerController  clone = Instantiate(player).GetComponent<PlayerController>();
+			PlayerController clone = Instantiate(player).GetComponent<PlayerController>();
 
 			// takes care of creating a playercontroller instance on frame 1 that other scripts are looking for
 			PlayerController.instance = clone;
