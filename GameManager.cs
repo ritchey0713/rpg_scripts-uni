6d4531cd9fa1c364cdde714de82411fa1c9ebb71
maveryk <ritchey0713@gmail.com>
2021-02-08 16:33:03 -0500

    add fix for player moving

--- GameManager.cs
+++ GameManager.cs
@@ -7,6 +7,8 @@ public class GameManager : MonoBehaviour {
 
 	public static GameManager instance;
 
+	public bool gameMenuOpen, dialogActive, fadingBetweenAreas;
+
 	public CharStats[] playerStats;
 	void Start() {
 		if(instance == null){
--- GameMenu.cs
+++ GameMenu.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameMenu : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class GameMenu : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public GameObject theMenu;
+	// Start is called before the first frame update
+	void Start() {
+		
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if(Input.GetButtonDown("Fire2")){
+			Debug.Log("test");
+			if(theMenu.activeInHierarchy){
+				theMenu.SetActive(false);
+				PlayerController.instance.canMove = true;
+			} else {
+				theMenu.SetActive(true);
+				PlayerController.instance.canMove = false;
+			}
+		}	
+	}
 }
