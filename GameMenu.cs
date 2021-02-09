239b80cf254e6463b14eb0a665e8fc796b537999
maveryk <ritchey0713@gmail.com>
2021-02-09 13:11:23 -0500

    add menu stats updating

--- GameMenu.cs
+++ GameMenu.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,6 +15,8 @@ public class GameMenu : MonoBehaviour {
 	public Slider[] expSlider;
 
 	public Image[] charImage; 
+
+	public GameObject[] charStatHolder;
 	// Start is called before the first frame update
 	void Start() {
 		
@@ -21,20 +24,32 @@ public class GameMenu : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update() {
-		if(Input.GetButtonDown("Fire2")){
-			Debug.Log("test");
+		if(Input.GetButtonDown("Fire2"))
 			if(theMenu.activeInHierarchy){
+
 				theMenu.SetActive(false);
 				GameManager.instance.gameMenuOpen = false;
+
 			} else {
+
 				theMenu.SetActive(true);
 				UpdateMainStats();
 				GameManager.instance.gameMenuOpen = true;
+
 			}
 		}	
-	}
 
 	public void UpdateMainStats(){
 		playerStats = GameManager.instance.playerStats;
+		for(int i = 0; i < playerStats.Length; i++){
+
+			// playerStats[i].gameObject.activeInHierarchy => looks at prefab to see if a char is deactivated, if they are when the menu opens they will not show, 
+			if(playerStats[i].gameObject.activeInHierarchy){
+				charStatHolder[i].SetActive(true);
+			} else {
+				charStatHolder[i].SetActive(false);
+			}
+		}
 	}
+
 }
