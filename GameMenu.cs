82618f57b6b94d2f8fb8a1ffd0878861a0265fd4
maveryk <ritchey0713@gmail.com>
2021-02-09 15:54:54 -0500

    work out menu bug

--- GameMenu.cs
+++ GameMenu.cs
@@ -8,6 +8,8 @@ public class GameMenu : MonoBehaviour {
 
 	public GameObject theMenu;
 
+	public GameObject[] windows;
+
 	private CharStats[] playerStats;
 
 	public Text[] nameText, hpText, mpText, lvlText, expText;
@@ -40,14 +42,37 @@ public class GameMenu : MonoBehaviour {
 		}	
 
 	public void UpdateMainStats(){
+		// get most up to date stats from the game manager
+		// game manager keeps track of the stats for everyone via the playerStats arr
 		playerStats = GameManager.instance.playerStats;
 		for(int i = 0; i < playerStats.Length; i++){
 
 			// playerStats[i].gameObject.activeInHierarchy => looks at prefab to see if a char is deactivated, if they are when the menu opens they will not show, 
 			if(playerStats[i].gameObject.activeInHierarchy){
 				charStatHolder[i].SetActive(true);
+				
+				// these ref the menu items assigned
+				nameText[i].text = playerStats[i].charName;
+				hpText[i].text = "HP: " + playerStats[i].currentHP + "/" + playerStats[i].maxHP;
+				mpText[i].text = "MP: " + playerStats[i].currentMP + "/" + playerStats[i].maxMP;
+				lvlText[i].text = "Lvl: " + playerStats[i].playerLevel;
+				expText[i].text = "" + playerStats[i].currentExp + "/" + playerStats[i].expToNextLevel[playerStats[i].playerLevel];
+				expSlider[i].maxValue = playerStats[i].expToNextLevel[playerStats[i].playerLevel];
+				expSlider[i].value = playerStats[i].currentExp;
+				charImage[i].sprite = playerStats[i].charImage;
 			} else {
 				charStatHolder[i].SetActive(false);
+			}
+		}
+	}
+
+	public void ToggleWindow(int windowNumber){
+		Debug.Log("test");
+		for(int i = 0; i < windows.Length; i++){
+			if(i == windowNumber){
+				windows[i].SetActive(!windows[i].activeInHierarchy);
+			} else {
+				windows[i].SetActive(false);
 			}
 		}
 	}
