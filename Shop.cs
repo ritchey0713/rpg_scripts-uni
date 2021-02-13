928226458b8643805c9fdc92a1ca88d2f5ca7870
maveryk <ritchey0713@gmail.com>
2021-02-13 18:02:26 -0500

    flesh out listing items in buy/sell windows

--- Shop.cs
+++ Shop.cs
@@ -16,6 +16,12 @@ public class Shop : MonoBehaviour {
 
 	public Text goldText;
 
+	public string[] itemsForSale;
+
+	public ItemButton[] buyItemButtons;
+
+	public ItemButton[] sellItemButtons;
+
 	void Start() {
 		instance = this;
 
@@ -46,10 +52,40 @@ public class Shop : MonoBehaviour {
 	public void OpenBuyMenu(){
 		buyMenu.SetActive(true); 
 		sellMenu.SetActive(false);
+
+		for(int i = 0; i < buyItemButtons.Length; i++){
+			buyItemButtons[i].buttonValue = i;
+			if(itemsForSale[i] != ""){
+				//show button
+				buyItemButtons[i].buttonImage.gameObject.SetActive(true);
+				// access gamemanager => it tracks the items held and a ref to all items that we add to the game and the number of how many we have of each item
+				buyItemButtons[i].buttonImage.sprite = GameManager.instance.GetItemDetails(itemsForSale[i]).itemSprite;
+				buyItemButtons[i].amountText.text = "";
+			} else {
+				// hide the button
+				buyItemButtons[i].buttonImage.gameObject.SetActive(false);
+				buyItemButtons[i].amountText.text = "";
+			}
+		}	
 	}
 
 	public void OpenSellMenu(){
 		buyMenu.SetActive(false); 
 		sellMenu.SetActive(true);
+
+		for(int i = 0; i < sellItemButtons.Length; i++){
+			sellItemButtons[i].buttonValue = i;
+			if(GameManager.instance.itemsHeld[i] != ""){
+				//show button
+				sellItemButtons[i].buttonImage.gameObject.SetActive(true);
+				// access gamemanager => it tracks the items held and a ref to all items that we add to the game and the number of how many we have of each item
+				sellItemButtons[i].buttonImage.sprite = GameManager.instance.GetItemDetails(GameManager.instance.itemsHeld[i]).itemSprite;
+				sellItemButtons[i].amountText.text = GameManager.instance.numberOfItems[i].ToString();
+			} else {
+				// hide the button
+				sellItemButtons[i].buttonImage.gameObject.SetActive(false);
+				sellItemButtons[i].amountText.text = "";
+			}
+		}
 	}
 }
--- Shopkeeper.cs
+++ Shopkeeper.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shopkeeper : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Shopkeeper : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private bool canOpen;
+
+	public string[] itemsForSale = new string[40];
+	// Start is called before the first frame update
+	void Start() {
+			
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if(canOpen && Input.GetButtonDown("Fire1") && PlayerController.instance.canMove && !Shop.instance.shopMenu.activeInHierarchy){
+			//list items for sale;
+			Shop.instance.itemsForSale = itemsForSale;
+			Shop.instance.OpenShop();
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		if(other.tag == "Player"){
+			canOpen = true;
+		}
+	}
+
+		private void OnTriggerExit2D(Collider2D other) {
+		if(other.tag == "Player"){
+			canOpen = false;
+		}
+	}
 }
