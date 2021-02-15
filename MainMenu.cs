07aa485466a7fc04fedd032b01dd1bb32537a61f
maveryk <ritchey0713@gmail.com>
2021-02-15 17:40:31 -0500

    add add toggle for continue button

--- MainMenu.cs
+++ MainMenu.cs
@@ -1,15 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 	// Start is called before the first frame update
+
+	public string newGameScene;
+
+	public GameObject continueButton;
 	void Start() {
-			
+		if(PlayerPrefs.HasKey("Current_Scene")){
+			continueButton.SetActive(true);
+		} else {
+			continueButton.SetActive(false);
+		}
 	}
 
 	// Update is called once per frame
 	void Update() {
 			
+	}
+
+	public void Continue(){
+
+	}
+
+	public void NewGame(){
+		SceneManager.LoadScene(newGameScene);
+	}
+
+	public void Exit(){
+		Application.Quit();
 	}
 }
